6278bca5ca497bebcc582af1b168cc7cac10e0a2
Isai Salgado <joaquin.salgado@ymail.com>
2021-04-17 14:57:08 -0600

    Create usuario.go

---
+++ models/usuario.go
@@ -0,0 +1,22 @@
+package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+/*Usuario es el Modeo de usuario de la base de mongodb*/
+type Usuario struct {
+	ID              primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Nombre          string             `bson:"nombre" json:"nombre,omitempty"`
+	Apellidos       string             `bson:"apellidos" json:"apellidos,omitempty"`
+	FechaNacimiento time.Time          `bson:"fechaNacimiento" jso:"fechaNacimiento,mitempty"`
+	Email           string             `bson:"email" json:"email"`
+	Password        string             `bson:"password" json:"passord,omitempty"`
+	Avatar          string             `bson:"avatar" json:"avatar,omitempty"`
+	Banner          string             `bson:"banner" json:"banner,omitempty"`
+	Biografia       string             `bson:"biografia" json:"biografia,omitempty"`
+	Ubicacion       string             `bson:"ubicacion" json:"ubicacion,omitempty"`
+	SitioWeb        string             `bson:"sitioWeb" json:"sitioWeb,omitempty"`
+}
