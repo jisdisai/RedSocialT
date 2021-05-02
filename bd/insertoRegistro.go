07d30367a79d9e32394add0cd63312a7c738bcf4
Isai Salgado <joaquin.salgado@ymail.com>
2021-05-02 13:58:20 -0600

    Creado insertoRegistro y encriptarPassword  donde esta la rutina de crear contraseña lo que es el encriptado

---
+++ bd/insertoRegistro.go
@@ -0,0 +1,29 @@
+package bd
+
+import (
+	"context"
+	"time"
+
+	"github.com/jisdisai/RedSocialT/models"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+func InsertoRegistro(u models.Usuario) (string, bool, error) {
+
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	db := MongoCN.Database("redsocialt")
+	col := db.Collection("usuarios")
+
+	u.Password, _ = EncriptarPassword(u.Password)
+
+	result, err := col.InsertOne(ctx, u)
+	if err != nil {
+		return "", false, err
+	}
+
+	objID, _ := result.InsertedID.(primitive.ObjectID)
+	return objID.String(), true, nil
+
+}
