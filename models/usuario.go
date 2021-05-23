fb939e72a5fc020394c129cf201d33ba575bc350
Isai Salgado <joaquin.salgado@ymail.com>
2021-05-22 18:13:13 -0600

    Se Creo procesoToken y se modificaron en usuario claim valido falta arreglar 2 erroresen proceso token

--- models/usuario.go
+++ models/usuario.go
@@ -11,7 +11,7 @@ type Usuario struct {
 	ID              primitive.ObjectID `bson:"_id,omitempty" json:"id"`
 	Nombre          string             `bson:"nombre" json:"nombre,omitempty"`
 	Apellidos       string             `bson:"apellidos" json:"apellidos,omitempty"`
-	FechaNacimiento time.Time          `bson:"fechaNacimiento" jso:"fechaNacimiento,mitempty"`
+	FechaNacimiento time.Time          `bson:"fechaNacimiento" json:"fechaNacimiento,mitempty"`
 	Email           string             `bson:"email" json:"email"`
 	Password        string             `bson:"password" json:"passord,omitempty"`
 	Avatar          string             `bson:"avatar" json:"avatar,omitempty"`
---
+++ routers/procesoToken.go
@@ -0,0 +1,47 @@
+package routers
+
+import (
+	"errors"
+	"strings"
+
+	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/jisdisai/RedSocialT/bd"
+	"github.com/jisdisai/RedSocialT/models"
+)
+
+/*Email valor de email Usado todos los EndPoints */
+var Email string
+
+/* IDUsuario es el ID devuelto del modelo, que se usara en todos los EndPoints */
+var IDUsuario string
+
+/*ProcesoToken proceso para extraer sus valores */
+func ProcesoToken(tk string) (*models.Claim, bool, string, error) {
+
+	miClave := []byte("Jehova777")
+	claims := &models.Claim{}
+
+	splitToken := strings.Split(tk, "Bearer")
+	if len(splitToken) != 2 {
+		return claims, false, string(""), errors.New("formato de token invalido")
+	}
+
+	tk = string.TrimSpace(splitToken[1])
+
+	tkn, err := jwt.ParseWithClaims(tk, claims, func(token *jwt.Token) (interface{}, error) {
+		return miClave, nil
+	})
+	if err == nil {
+		_, encontrado, ID := bd.ChequeoYaExisteUsuario(claims.Email)
+		if encontrado == true {
+			Email = claims.Email
+			IDUsuario = claims.ID.Hex()
+		}
+		return claims, encontrado, IDUsuario, nil
+	}
+	if !tkn.Valid {
+		return claims, false, string(""), errors.New("token Invalido")
+	}
+	return claims, false, string(""), err
+
+}
