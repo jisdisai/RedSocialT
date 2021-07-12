c48547f8a4f80f327ecaf96a67d06778f964553f
Isai Salgado <joaquin.salgado@ymail.com>
2021-07-12 12:59:19 -0600

    se creo Modificar Perfil en los registros y routers

---
+++ routers/modificarPerfil.go
@@ -0,0 +1,35 @@
+package routers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/jisdisai/RedSocialT/bd"
+	"github.com/jisdisai/RedSocialT/models"
+)
+
+/* ModificarPerfil modifica el perfil de usuario */
+func ModificarPerfil(w http.ResponseWriter, r *http.Request) {
+
+	var t models.Usuario
+
+	err := json.NewDecoder(r.Body).Decode(&t)
+	if err != nil {
+		http.Error(w, "Datos Incorrectos"+err.Error(), 400)
+		return
+	}
+
+	var status bool
+	status, err = bd.ModificoRegistro(t, IDUsuario)
+	if err != nil {
+		http.Error(w, "Ocurrio un Error al Intentar Modificar el Registro. Intente Nuevamente"+err.Error(), 400)
+		return
+	}
+
+	if status == false {
+		http.Error(w, "No se ha Logrado modificar el usuario ", 400)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+}
